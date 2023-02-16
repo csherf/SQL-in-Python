21a6dbee4a1d4d9c0f73b6e625d5b100aa2b5870
csherf <chadnsherf@gmail.com>
2023-02-16 00:41:16 -0800

    allows for multiple commands on one line

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 #Date: 2/15/23
 
 # Keywords
+
 CREATE = "CREATE"
 DROP = "DROP"
 USE = "USE"
@@ -9,40 +10,67 @@ ALTER = "ALTER"
 SELECT = "SELECT"
 EXIT = ".EXIT"
 
+listKeywords = [CREATE, DROP, USE, ALTER, SELECT, EXIT]
+
 def main():
+    print("#------ Sql in Python Started -------#\n")
     while(1):
-        input = getInput()
-        parsedInput = parseInput()
-        for word in parsedInput:
-            if parsedInput[0].upper() == EXIT:
-                return exit()
-            elif testKeyword():
-                runCommand()
-            else:
-                invalidInput()
+        # get the commands from the users input. A user can enter multiple commands seperated by ;
+        #  example CREATE DATABASE db_1; CREATE DATABASE db_1;
+        command = getInput()
+        commands = command.split(";")
+
+        for command in commands:
+            parsedCommand = parseCommand(command)
+            
+            for word in parsedCommand:
+                if word.upper() == EXIT:
+                    return exit()
+                elif testKeyword(word):
+                    runCommand(parsedCommand)
+                else:
+                    print("invalid input\n")
 
 
 # Get the users input from terminal 
 def getInput():
     # type: () -> str
-    return "1"
+    userInput = input()
+    return userInput
 
 # parse the users input for key words and make sure its valid
-def parseInput(input):
+def parseCommand(command):
     # type: (str) -> list[str]
-    return [""]
+    parsedCommand = command.split()
+    return parsedCommand
 
 # test if its a valid keyword
 def testKeyword(keyword):
-     # type: (str) -> bool
+    # type: (str) -> bool
+    keyword = keyword.upper()
+    if listKeywords.count(keyword) != 0:
+       return 1
     return 0
 
 # run a given command 
 def runCommand(command):
-    # type: (str) -> bool
+    # type: (list[str]) -> bool
+    firstWord = command[0]
+    if firstWord == CREATE:
+        create(command)
+    if firstWord == DROP:
+        drop(command)
+    if firstWord == USE:
+        useDatabase(command)
+    if firstWord == ALTER:
+        alterTable(command)
+    if firstWord == SELECT:
+        select(command)
     return 0
 
-def invalidInput():
+
+# create a table or database
+def create(command):
     return 0
 
 # create a database (in this case a new folder)
@@ -53,6 +81,10 @@ def createDatabase():
 def createTable():
     return 0
 
+# run the commands needed for dropping a table or database
+def drop(command):
+    return 0
+
 # delete a database by deleting the folder asscoaited with it
 def dropDatabase():
     return 0
@@ -61,15 +93,16 @@ def dropDatabase():
 def dropTable():
     return 0
 
-#defines which db/folder the user is working out of
-def useDatabase():
+#d efines which db/folder the user is working out of
+def useDatabase(command):
     return 0
 
-#edits a given table (file)
+# edits a given table (file)
 def alterTable():
     return 0
 
-def select():
+# select query 
+def select(command):
     return 0
 
 #quit out of the application
@@ -77,4 +110,8 @@ def exit():
     return 0
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nClosing DB\n")
+        
